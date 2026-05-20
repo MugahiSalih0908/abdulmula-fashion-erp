69b5433babab8cd1e5c4ebb18a9cf7ef085b8325
Mugahid Salih Adam <mugahidsalihadam@gmail.com>
2026-05-20 18:57:35 +0530

    Fix email production config

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -8,7 +8,7 @@ const createTransporter = () =>
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,   // Google App Password – NOT Gmail password
+      pass: process.env.EMAIL_PASS,
     },
   });
 
